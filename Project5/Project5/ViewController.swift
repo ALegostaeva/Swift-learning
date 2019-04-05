4262ab16a5f018a6fdc87a85ad8234a0ff9e39d8
Svoboda <sashasbox.forjob@gmail.com>
2019-04-05 23:43:44 +0200

    project5 started

---
+++ Project5/Project5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by Александра Легостаева on 05/04/2019.
+//  Copyright © 2019 self. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
