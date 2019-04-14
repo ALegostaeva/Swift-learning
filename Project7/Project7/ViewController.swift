1bc92c88a097f0e23e32e19109a29960c9c44550
Svoboda <sashasbox.forjob@gmail.com>
2019-04-14 17:50:04 +0200

    project 7 - started

---
+++ Project7/Project7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Александра Легостаева on 14/04/2019.
+//  Copyright © 2019 self. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
