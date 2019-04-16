2328dea2346174377c0352c4e1f882d0d0142b9c
Svoboda <sashasbox.forjob@gmail.com>
2019-04-16 11:45:36 +0200

    project8 - started

---
+++ Project8/Project8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project8
+//
+//  Created by Александра Легостаева on 16/04/2019.
+//  Copyright © 2019 self. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
