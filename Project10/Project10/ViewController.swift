b2aa654a9d9d194998e09226b2245138f79746bf
Svoboda <sashasbox.forjob@gmail.com>
2019-04-19 20:48:13 +0200

    project10 - started

---
+++ Project10/Project10/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Александра Легостаева on 19/04/2019.
+//  Copyright © 2019 self. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
