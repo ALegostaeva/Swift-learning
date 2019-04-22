d8c855c8056be3cf6a911b56a22c950fb3176257
Svoboda <sashasbox.forjob@gmail.com>
2019-04-22 22:41:58 +0200

    project9 - updated project8 as a part of exta tasks in project9
    added GCD

--- Project8/Project8/ViewController.swift
+++ Project8/Project8/ViewController.swift
@@ -141,7 +141,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        loadLevel()
+        performSelector(inBackground: #selector(loadLevel), with: nil)
     }
     
     @objc func letterTapped(_ sender: UIButton) {
@@ -199,7 +199,7 @@ class ViewController: UIViewController {
         activatedButtons.removeAll()
     }
 
-    func loadLevel() {
+    @objc func loadLevel() {
         var clueString = ""
         var solutionString = ""
         var letterBits = [String]()
@@ -226,14 +226,17 @@ class ViewController: UIViewController {
             }
         }
         
-        cluesLabel.text = clueString.trimmingCharacters(in: .whitespacesAndNewlines)
-        answersLabel.text = solutionString.trimmingCharacters(in: .whitespacesAndNewlines)
-        
         letterBits.shuffle()
         
-        if letterBits.count == letterButtons.count {
-            for i in 0 ..< letterButtons.count {
-                letterButtons[i].setTitle(letterBits[i], for: .normal)
+        DispatchQueue.main.async { [weak self] in
+            self?.cluesLabel.text = clueString.trimmingCharacters(in: .whitespacesAndNewlines)
+            self?.answersLabel.text = solutionString.trimmingCharacters(in: .whitespacesAndNewlines)
+            
+            
+            if letterBits.count == self?.letterButtons.count {
+                for i in 0 ..< letterBits.count {
+                    self?.letterButtons[i].setTitle(letterBits[i], for: .normal)
+                }
             }
         }
     }
@@ -243,7 +246,7 @@ class ViewController: UIViewController {
         solutions.removeAll(keepingCapacity: true)
         usedWords.removeAll()
         
-        loadLevel()
+        performSelector(inBackground: #selector(loadLevel), with: nil)
         
         for btn in letterButtons {
             btn.isHidden = false
